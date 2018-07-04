10c191708cfbaf7335fa89ae7db54f53f164723d
Patryk Przybylski <PPrzybylsky@gmail.com>
2018-07-04 13:46:45 +0200

    Add files via upload
    3 new dataframes

--- email_tracker.py
+++ email_tracker.py
@@ -28,7 +28,7 @@ def SendEmail(to, subject, body, html):
     try:
         _mail = outlook.CreateItem(0)
         _mail.To = to
-        _mail.cc = 'patryk.przybylski@nokia.com;'  # bartosz.bieda@nokia.com;adrian.kuboszek@nokia.com'
+        _mail.cc = 'patryk.przybylski@nokia.com;bartosz.bieda@nokia.com;adrian.kuboszek@nokia.com'
         _mail.Subject = subject
         _mail.Body = body
         _mail.htmlBody = html
@@ -52,7 +52,7 @@ def UpdateExcelFile(worksheet, pm):
         i += 1
     return
 
-
+#colors
 def get_html_from_df(df: object, columns: object, color: object) -> object:
     columns_order = df.columns.values
     result = pd.DataFrame()
@@ -71,7 +71,7 @@ def get_html_from_df(df: object, columns: object, color: object) -> object:
 
 
 # setting up environment
-root = 'C:\\Users\\pprzybyl\\Desktop\\IPT\\Reports\\Monthly\\05. May'
+root = 'C:\\Users\\pprzybyl\\Desktop\\IPT\\Reports\\Monthly\\06. June'
 os.chdir(root)
 fileName = 'email_tracker.xlsx'
 
@@ -93,22 +93,27 @@ df = baseFile.loc[
 df['email'] = df['Pricing Manager'].apply(lambda x: str(x).replace(' ', '.') + '@nokia.com')
 df['Opportunity ID Desc'] = df['Opportunity ID Desc'].apply(lambda x: '' if x != x else x[1:])
 
-df.shape
-
 outlook = win32.Dispatch('outlook.application')  # start outlook application
 
-columns = ['Deal Id', 'Total Revenue', 'Customer name', 'Workteam', 'Opportunity ID Desc', 'Updated At',
-           'Pricing Manager']
-receivers = df.groupby('email')
-df = df[columns]
+columns = ['Deal Id', 'Total Revenue', 'Customer name', 'Workteam', 'Opportunity ID Desc', 'Updated At', 'Pricing Manager']
+
+# df = df[columns]
 invalid_customer = pd.DataFrame()
 invalid_opp_ID = pd.DataFrame()
-both = pd.DataFrame()
+invalid_both = pd.DataFrame()
+receivers = []
 
 for index, row in df.iterrows():
+    if row['Pricing Manager'] in receivers:
+        pass
+    else:
+        receivers.append(row['Pricing Manager'])
+
     if "Dummy Customer" in row["Customer name"]:
-        if "Dummy Customer" in row["Customer name"] and "wrong value" in row["Opportunity ID Desc"]:
-         invalid_customer_ = invalid_customer.append(row)
+        if "Dummy Customer" in row["Customer name"] and "wrong" in row["Opportunity ID Desc"]:
+            invalid_both = invalid_both.append(row)
+        elif "Dummy Customer" in row["Customer name"]:
+            invalid_customer = invalid_customer.append(row)
     #elif "Dummy Customer" in row["Customer name"] and "" in row["Opportunity ID Desc"]:
         #both = both.append(row)
     elif "" in row["Opportunity ID Desc"]:
@@ -116,37 +121,44 @@ for index, row in df.iterrows():
 
 invalid_opp_ID = invalid_opp_ID[columns]
 invalid_customer = invalid_customer[columns]
+invalid_both = invalid_both[columns]
 
-for receiver, data in receivers:
+path = r'C:\Users\pprzybyl\Desktop\IPT\Reports\Monthly\06. June'
+writer = pd.ExcelWriter('Email_Backlog.xlsx')
+
+for receiver in receivers:
     print(receiver + '\n')
+    invalid_customer_sent = invalid_customer[invalid_customer["Pricing Manager"] == receiver]
+    invalid_opp_ID_sent = invalid_opp_ID[invalid_opp_ID['Pricing Manager'] == receiver]
+    invalid_both_sent = invalid_both[invalid_both['Pricing Manager'] == receiver]
+
     _header = '<tr>\n' + Row2string(columns, True) + '\n</tr>\n'  # generate table head
     _text = "Hi, <br><br>  The following cases, which were uploaded to IPT didn't get to Hydra due to lack of Opportunity ID, Customer ID (Dummy Customer) or wrong Revenue.<br><br>\
-            There is a necessity to investigate that so please provide me with information on these cases below: <br>"
-    _text += "<br> - Invalid Customer Name has been detected in the cases with following Deal IDs and highlighted below:"
-    _text += get_html_from_df(invalid_customer, ['Customer name'], 'orange')
-    _text += "<br> - Invalid or lack of Opportunity ID has been detected in the cases with following Deal IDs and highlighted below: <br>"
-    _text += get_html_from_df(invalid_opp_ID, ['Opportunity ID Desc'], 'orange')
-    _text += get_html_from_df(both, ['Customer name','Opportunity ID Desc'], 'orange')
+             There is a necessity to investigate that so please provide me with information on these cases below: <br>"
+    if not invalid_customer_sent.empty:
+        print(invalid_customer.head(n=1))
+        _text += "<br> <b>- Invalid Customer Name has been detected in the cases with the following Deal IDs and highlighted below:</b>"
+        _text += get_html_from_df(invalid_customer_sent, ['Customer name'], 'orange')
+    if not invalid_opp_ID_sent.empty:
+        _text += "<br> <b>- Invalid or lack of Opportunity ID has been detected in the cases with the following Deal IDs and highlighted below:</b> <br>"
+        _text += get_html_from_df(invalid_opp_ID_sent, ['Opportunity ID Desc'], 'orange')
+    if not invalid_both_sent.empty:
+        _text += "<br> <b>- Invalid or lack Opportunity ID and Customer Name has been detected in the cases with the following Deal IDs and highlighted below: </b> <br>"
+        _text += get_html_from_df(invalid_both_sent, ['Customer name','Opportunity ID Desc'], 'orange')
+    _text += "<br><br> Best regards, <br> Patryk Przybylski"
 
-    _text += "'<br><br> Best regards, <br> Patryk Przybylski"
-    #footer = '<br><br> Best regards.'
-
-    _htmlbody = ''
-    # generating table content
-    # for _index, _row in data[columns].iterrows():
-    #     _rowHTML = '<tr>\n' + Row2string(_row) + '</tr>\n'
-    #     _htmlbody += _rowHTML
-
-    # finalize email
-    # _htmlbody = '<table border="1" class="dataframe">\n' + _header + _htmlbody + '</table>'  # create final table
-    _emailSent = SendEmail(to=str(receiver), subject='Invalid IPT Cases - 05/2018', body='', html=_text + _htmlbody)
+    # for index, row in data[columns].iterrows():
+    _emailSent = SendEmail(to=str(receiver), subject='Invalid IPT Cases - 06/2018', body='', html=_text)
 
     if _emailSent:
-        print('Updating excel file...')
-        UpdateExcelFile(worksheet=ws, pm=_row['Pricing Manager'])
+        baseFile.loc[(baseFile['Pricing Manager'] == receiver), "sent"] = 'x'
 
-    print('--------\n')
+    invalid_customer_sent = None
+    invalid_opp_ID_sent = None
+    invalid_both_sent = None
 
-wb.save('email_tracker_' + DateToString() + '.xlsx')
+baseFile.to_excel(writer, index=False)
+writer.save()
+# wb.save('email_tracker_' + DateToString() + '.xlsx')
 outlook = None
-wb = None
+# wb = None
